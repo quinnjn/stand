3261e63be27cd0c9ca89961a587fe3bb293abe2b
quinnjn <quinn@neumiiller.com>
2016-01-22 11:18:54 -0800

    App.java converted to Kotlin

---
+++ app/src/main/java/com/neumiiller/stand/App.kt
@@ -0,0 +1,22 @@
+package com.neumiiller.stand
+
+import android.app.Application
+import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+/**
+ * Created by qneumiiller on 1/22/16.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        connectCrashlytics()
+    }
+
+    private fun connectCrashlytics() {
+        Fabric.with(this, Crashlytics())
+    }
+
+}
